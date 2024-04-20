9a6c595572f69e11a93ecc1930cd1871256f6a97
Dev <flashytesting@gmail.com>
2024-04-20 19:58:07 +0530

    Add server changes for file API

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
-import pool from './db/pool.js';
+
+// Routes
+import { storageRoutes } from './routes/storage.js';
+import { subjectRouter } from './routes/subject.js';
 
 const app = express();
 
-app.get('/db', async (req, res) => {
-	try {
-		await pool.query(`CREATE TABLE IF NOT EXISTS computer_network (
-			id SERIAL PRIMARY KEY,
-			file_name VARCHAR(255) UNIQUE NOT NULL,
-			file_data BYTEA
-		)`)
+app.use('/subjects', subjectRouter);
+app.use('/storage', storageRoutes);
 
-		res.json({ message: '\'coputer_network\' table created.' });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error });
-	}
-});
-
-app.get('/health', (req, res) => {
-	res.json({ message : 'OK' });
+app.get('/health', (_req, res) => {
+    res.json({ message: 'OK' });
 })
 
 app.listen(3000, () => {
-	console.log("Server listening on port 3000");
+    console.log("Server listening on port 3000");
 })
